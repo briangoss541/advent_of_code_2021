821fb066927251e3689b3b232cf18185f75def9a
briangoss541 <35881754+briangoss541@users.noreply.github.com>
2021-12-06 15:23:48 -0500

    Nailed it

---
+++ 6/6_2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+sample_input = '3,4,3,1,2'
+
+with open('input.txt') as in_file:
+    in_data = [x.strip() for x in in_file.readlines()]
+
+initial_fishes = [int(x) for x in in_data[0].split(',')]
+
+fish_ladder = defaultdict(int)
+
+# Instantiate fish
+for initial_timer in initial_fishes:
+    fish_ladder[initial_timer] += 1
+
+for day in range(1, 256 + 1):
+
+    day_ladder = defaultdict(int)
+
+    for timer_value, population_count in fish_ladder.items():
+
+        if timer_value == 0:
+            day_ladder[6] += population_count
+            day_ladder[8] += population_count
+        else:
+            day_ladder[timer_value - 1] += population_count
+
+    fish_ladder = day_ladder
+
+print(fish_ladder)
+print(sum([x for x in fish_ladder.values()]))
